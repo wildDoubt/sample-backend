2f2abca534f7abe075581eca64b2dbba90f93f87
wildDoubt <coma01155@gmail.com>
2023-01-05 08:37:49 +0900

    fix: 빌드 스크립트 수정

--- build.sh
+++ build.sh
@@ -9,7 +9,7 @@ read -p "데이터베이스 URL 입력: " DB_URL
 read -p "데이터베이스 사용자 이름 입력: " DB_USERNAME
 read -sp "데이터베이스 암호 입력: " DB_PASSWORD
 
-echo "database.DB_URL=$DB_URL" >> ./src/main/resources/database.properties
+echo "database.DB_URL=jdbc:mysql://$DB_URL" >> ./src/main/resources/database.properties
 echo "database.DB_USERNAME=$DB_USERNAME" >> ./src/main/resources/database.properties
 echo "database.DB_PASSWORD=$DB_PASSWORD" >> ./src/main/resources/database.properties
 
