abeb4838acf73b79619b19fea0857f9492f9b955
wildDoubt <coma01155@gmail.com>
2023-01-04 14:09:09 +0900

    refactor: jakarta -> javax로 변경

--- src/main/java/xyz/wonyeong/api/dto/ToDo.java
+++ src/main/java/xyz/wonyeong/api/dto/ToDo.java
@@ -1,10 +1,11 @@
 package xyz.wonyeong.api.dto;
 
-import jakarta.persistence.*;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import javax.persistence.*;
 
 @Getter
 @Setter
