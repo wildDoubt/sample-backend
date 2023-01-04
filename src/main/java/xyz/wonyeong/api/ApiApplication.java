1bd9e094264b4aba961252da18f549006c4bf41a
wildDoubt <coma01155@gmail.com>
2023-01-04 14:08:14 +0900

    refactor: 자바 버전 다운그레이드
    tomcat에서 사용는 jdk 버전에 맞춰야 에러가 발생하지 않음

--- src/main/java/xyz/wonyeong/api/ApiApplication.java
+++ src/main/java/xyz/wonyeong/api/ApiApplication.java
@@ -2,10 +2,15 @@ package xyz.wonyeong.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 public class ApiApplication extends SpringBootServletInitializer {
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(ApiApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiApplication.class, args);
